017f7baa8e6809b309b1550922e4af183c7d8026
kweku76 <daffran@gmail.com>
2020-03-20 23:29:41 +1000

    Updates

---
+++ precache-manifest.c606312c6cd36c2efa0629a49d9a9c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65f8624b66be4e17dd1a0161f333dd3",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "bedc874175524253d359",
+    "url": "/pokedex/static/css/main.9616309a.chunk.css"
+  },
+  {
+    "revision": "764e478ef68828b70dd5",
+    "url": "/pokedex/static/js/2.689160e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.689160e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedc874175524253d359",
+    "url": "/pokedex/static/js/main.0bd6d2a5.chunk.js"
+  },
+  {
+    "revision": "c0a268ae7cfa02c084f2",
+    "url": "/pokedex/static/js/runtime-main.fa559e03.js"
+  }
+]);
